33311ef798d34ee9bcdd1ae5311394bce89d2f57
nnawar706 <nnawar706@gmail.com>
2024-02-08 00:44:01 +0600

    showing border on live chat

--- components/cursor/Cursor.tsx
+++ components/cursor/Cursor.tsx
@@ -6,6 +6,10 @@ const Cursor = ({ color, position, message }: CursorProps) => {
         <div className="pointer-events-none absolute top-0 left-0"
         style={{ transform: `translateX(${position.x}px) translateY(${position.y}px)` }}>
             <CursorSVG color={color}/>
+            {message && 
+            <div className="absolute left-2 top-5 bg-purple-500 px-4 py-2 text-sm leading-relaxed 
+                text-white rounded-[20px]"><p className="w-60 text-white">{message}</p></div>
+            }
         </div>
     )
     }
